7bd4ceeedb606b102e3749cb5f61fd02fe0c5fe2
kaivladimirv <kaivladimirv@gmail.com>
2023-04-11 09:59:28 +0600

    chore: Add logging for code debugging

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
@@ -25,9 +26,11 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+        Log::debug(__METHOD__ . ': 1');
         $request->authenticate();
-
+        Log::debug(__METHOD__ . ': 2');
         $request->session()->regenerate();
+        Log::debug(__METHOD__ . ': 3');
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
