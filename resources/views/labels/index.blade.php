9740fb315c97912b91b63e7a1cc3983ea568a391
kaivladimirv <kaivladimirv@gmail.com>
2023-05-03 12:01:45 +0600

    refactor: Adjusting the output of alerts

--- resources/views/labels/index.blade.php
+++ resources/views/labels/index.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
 
     <section class="bg-white">
-        <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+        <div class="max-w-screen-xl px-4 pt-28 pb-8 mx-auto lg:pb-16">
             @include('flash::message')
             <div class="grid col-span-full">
                 <h1 class="mb-5">{{ __('Labels') }}</h1>
